dcc53c7f62806b34185ad44d56a00b99a15d561d
Pedro Henrique <pedro_henriquegoncalves@outlook.com>
2024-01-08 19:43:41 -0300

    innitial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AgeToDays;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Este é um aplicativo que transforma anos em dias");
+        int inputYears = Convert.ToInt32(Console.ReadLine());
+        int outputDays = AgeConvert.AgeintoDays(inputYears);
+        Console.WriteLine($"Result: [{outputDays}] days!");
+    }
+}
+
+public static class AgeConvert
+{
+    public static int AgeintoDays (int years)
+    {
+        return 365 * years;
+    }
+}
